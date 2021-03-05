d2e2f5f111a8f691c35a224d4c76b9facd1eb0eb
Joan Mora <joanmora@enti.cat>
2021-03-05 10:45:41 +0100

    Add game master

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    public int numberOfAsteroids = 0;
+    public GameObject asteroid = null;
+    public int halfWidth = 0;
+    public int halfHeight = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (asteroid != null)
+        {
+            for (int i = 0; i < numberOfAsteroids; ++i)
+            {
+                Vector2 asteroidPosition = new Vector2(Random.Range(-halfWidth, halfWidth), Random.Range(-halfHeight, halfHeight));
+
+                GameObject.Instantiate(asteroid, asteroidPosition, new Quaternion());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
